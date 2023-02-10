6defe22bf5e3d958c8740c3a99a052ce377dd714
Bob Kelso <49724332+1208kelly@users.noreply.github.com>
2023-02-10 22:15:25 +0000

    updated functions to operate as expected

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function clearInput() {
-    document.getElementById("output").innerHTML = "0";
+    document.getElementById("output-display").innerHTML = "0";
 }
 
 function removeZero() {
-    var value = document.getElementById("output").innerHTML;
+    var value = document.getElementById("output-display").innerHTML;
     if (value == "0") {
          value = " "
-         document.getElementById("output").innerHTML = value;
+         document.getElementById("output-display").innerHTML = value;
     }
 }
 
 function percent() {
-    var value = document.getElementById("output").innerHTML;
+    var value = document.getElementById("output-display").innerHTML;
     value = value / 100;
-    document.getElementById("output").innerHTML = value;
+    document.getElementById("output-display").innerHTML = value;
 }
 
-function numberDisplay(value) {
+function numberDisplay(myvalue) {
     removeZero()
-    document.getElementById("output").innerHTML += value;
+    document.getElementById("output-display").innerHTML += myvalue;
 }
 
 function solve() {
-    removeZero()
-    var equation = document.getElementById("output").innerHTML;
-    var solved = eval(equation);
-    document.getElementById('output').innerHTML = solved;
+   removeZero()
+   var equation = document.getElementById("output-display").innerHTML;
+   var solved = eval(equation);
+   document.getElementById('output-display').innerHTML = solved;
 }
