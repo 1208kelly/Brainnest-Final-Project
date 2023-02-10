fd3d4613e0a7ad29d25c2d065930fe4c31817534
Bob Kelso <49724332+1208kelly@users.noreply.github.com>
2023-02-10 21:36:51 +0000

    adding fucntio to remove the 0 at the start of input

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function clearInput() {
     document.getElementById("output").innerHTML = "0";
 }
+
+function removeZero() {
+    var value = document.getElementById("output").innerHTML;
+    if (value == "0") {
+         value = " "
+         document.getElementById("output").innerHTML = value;
+    }
+}
