1908f0f7296878db25e0c762710b451bc31030ee
Bob Kelso <49724332+1208kelly@users.noreply.github.com>
2023-02-11 15:08:18 +0000

    added comments to script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Function that will clear the input back to 0
 function clearInput() {
     document.getElementById("output-display").innerHTML = "0";
 }
 
+// Function that will remove the 0 from the begining of input
 function removeZero() {
     var value = document.getElementById("output-display").innerHTML;
     if (value == "0") {
@@ -10,17 +12,20 @@ function removeZero() {
     }
 }
 
+// Function that displays percenatge
 function percent() {
     var value = document.getElementById("output-display").innerHTML;
     value = value / 100;
     document.getElementById("output-display").innerHTML = value;
 }
 
+// Function that displays the number input
 function numberDisplay(myvalue) {
     removeZero()
     document.getElementById("output-display").innerHTML += myvalue;
 }
 
+// Function that solves the input when = is pressed
 function solve() {
     removeZero()
     var equation = document.getElementById("output-display").innerHTML;
